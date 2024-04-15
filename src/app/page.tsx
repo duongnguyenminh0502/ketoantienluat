9e3c54f0a2b918cecef730274fdbc8e4cc892c4e
duongnguyenminh <duongnguyenminh0502@gmail.com>
2024-04-15 22:39:39 +0700

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,21 @@ export default function Home() {
   // 1018 × 22786
   return (
     <div className="flex justify-center bg-white">
-      <Image
-        alt="full-page"
-        src="/page.png"
-        width={1018}
-        height={22786}
-        unoptimized
-      />
+      <a
+        aria-label="Phone Number"
+        target="_blank"
+        rel="noopener"
+        href="tel:0339597989"
+        className="contents"
+      >
+        <Image
+          alt="full-page"
+          src="/page.png"
+          width={1018}
+          height={22786}
+          unoptimized
+        />
+      </a>
     </div>
   );
 }
